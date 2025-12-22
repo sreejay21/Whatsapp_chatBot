a2ed1e673a6c84aeadc737968b0fa057b9052238
Sreejay Shreekumar <sreejay.s@zerone-consulting.com>
2025-12-22 17:54:36 +0530

    code modification

--- src/repositories/whatsapp.repository.js
+++ src/repositories/whatsapp.repository.js
@@ -1,44 +1,46 @@
-const axios = require('axios')
-const FormData = require('form-data')
-const fs = require('fs')
-const WhatsappMessage = require('../models/whatsappMessage.model')
-require('dotenv').config()
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
+const WhatsappMessage = require('../models/whatsappMessage.model');
+require('dotenv').config();
 
-const BASE_URL = `${process.env.WHATSAPP_BASE_URL}/${process.env.WHATSAPP_API_VERSION}/${process.env.WHATSAPP_PHONE_NUMBER_ID}`
+const BASE_URL = `${process.env.WHATSAPP_BASE_URL}/${process.env.WHATSAPP_API_VERSION}/${process.env.WHATSAPP_PHONE_NUMBER_ID}`;
+
+const createMessagePayload = (to, type, content) => ({ to, type, [type]: content });
 
 const sendMessage = async (payload) => {
   const response = await axios.post(`${BASE_URL}/messages`, payload, {
     headers: {
       Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
-      'Content-Type': 'application/json'
-    }
-  })
+      'Content-Type': 'application/json',
+    },
+  });
 
-  await WhatsappMessage.create({
+  const messageData = {
     to: payload.to,
     type: payload.type,
     requestPayload: payload,
     responsePayload: response.data,
-    whatsappMessageId: response.data.messages?.[0]?.id
-  })
+    whatsappMessageId: response.data.messages?.[0]?.id,
+  };
 
-  return response.data
-}
+  return await WhatsappMessage.create(messageData);
+};
 
 const uploadImage = async (filePath) => {
-  const data = new FormData()
-  data.append('messaging_product', 'whatsapp')
-  data.append('file', fs.createReadStream(filePath))
-  data.append('type', 'image/png')
+  const data = new FormData();
+  data.append('messaging_product', 'whatsapp');
+  data.append('file', fs.createReadStream(filePath));
+  data.append('type', 'image/png');
 
   const response = await axios.post(`${BASE_URL}/media`, data, {
     headers: {
       Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
-      ...data.getHeaders()
-    }
-  })
+      ...data.getHeaders(),
+    },
+  });
 
-  return response.data
-}
+  return response.data;
+};
 
-module.exports = { sendMessage, uploadImage }
+module.exports = { createMessagePayload, sendMessage, uploadImage };
