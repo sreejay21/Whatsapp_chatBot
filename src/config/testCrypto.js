46e9b7ebe19d5f0891bbe694fd748b4be57fe892
Sreejay Shreekumar <sreejay.s@zerone-consulting.com>
2026-03-10 11:28:11 +0530

    Removed teh unwanted files -10March2026

--- src/config/testCrypto.js
+++
@@ -1,8 +0,0 @@
-const { decrypt, encrypt} = require("./crypto.util");
-
-// const result = encrypt("+917736053206");
-// console.log("Encrypted Text:", result);
-
-const original = decrypt("NqAPAXGBxO4nJTY3rMKjcg==");
-console.log("Decrypted Text:", original);
-
